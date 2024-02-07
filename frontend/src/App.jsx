88d5f066729d1a2d9e294c21c9ebe601d7e49d20
Harnoor singh <singhharnoor116@gmail.com>
2024-02-07 22:36:44 +0530

    Components and pages added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,21 @@
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Signup } from "./pages/Signup";
+import { Signin } from "./pages/Signin";
+import { SendMoney } from "./pages/SendMoney";
+import { Dashboard } from "./pages/Dashboard";
 function App() {
-
   return (
-    <div>
-        Hello world
-    </div>
-  )
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/send" element={<SendMoney />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/components/Heading.jsx
@@ -0,0 +1,4 @@
+// eslint-disable-next-line react/prop-types
+export function Heading({ label }) {
+  return <div className="font-bold text-4xl pt-6">{label}</div>;
+}
